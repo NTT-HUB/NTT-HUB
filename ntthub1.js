093aba0215d06dcbccfcc4fe849474cf257c96c4
NTT-HUB <thaitrucfb3@gmail.com>
2024-10-01 17:15:45 +0700

    Update ntthub1.js

--- ntthub1.js
+++ ntthub1.js
@@ -58,7 +58,17 @@ function sendPost() {
             setTimeout(() => {
                 if (data.status === true) {
                     console.log('POST thành công, chuyển hướng...');
-                    // Nếu thành công, chuyển tới link của ngày 1
+              const hwid = localStorage.getItem('savehwid');      
+                    
+            if (!hwid) {
+            localStorage.setItem('hwid', hwid);
+        // Hiển thị thông báo khi HWID được lưu
+            console.log('HWID đã được lưu:', hwid);
+        } else {
+            console.log('Không tìm thấy HWID trong URL');
+        }
+                
+
                     window.location.href = redirectLink;
                 } else {
                     console.log('POST không thành công, chuyển về trang mặc định...');
