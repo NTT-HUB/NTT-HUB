6a4a4bcaa6932ff30782d7907884eb9b5f0134c9
NTT-HUB <thaitrucfb3@gmail.com>
2024-10-02 19:44:39 +0700

    Update ntthub.js

--- ntthub.js
+++ ntthub.js
@@ -7,6 +7,7 @@ if (savehwid) {
     console.log('HWID đã được lưu:', savehwid);
 } else {
     console.log('Không tìm thấy HWID trong URL');
+    alert('Don't Have HWID!');
 }
 
 function buttonClicked1() {
